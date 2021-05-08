3636dd3d88d18dbfc9334b4e63c1a4b0940fabf6
laurajunco <lc.junco10@uniandes.edu.co>
2021-05-08 15:25:57 -0500

    Added page transitions

--- 01_ava-sherry/scripts.js
+++ 01_ava-sherry/scripts.js
@@ -5,7 +5,29 @@ barba.use(barbaCss)
 barba.init({
   transitions: [
     {
-      name: "fade"
+      name: "fade",
+      beforeEnter({ current, next, trigger })  {
+        scrolltop()
+        selectNav(next)
+      }
     }
   ]
 })
+
+const scrolltop = () => {
+  window.scrollTo({top: 0,behavior: "smooth"})
+}
+
+const selectNav = (next) => {
+  const headerLinks = document.querySelectorAll("nav a")
+  const href = next.url.path
+
+  headerLinks.forEach(link => {
+    
+    if(link.getAttribute("href") === href) {
+      link.classList.add("selected")
+    } else {
+      link.classList.remove("selected")
+    }
+  })
+}
