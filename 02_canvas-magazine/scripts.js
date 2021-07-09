9cf9f448293c884497ae3c98aa7fa78c34124f12
Laura Junco <lc.junco10@uniandes.edu.co>
2021-07-09 15:59:41 -0500

    Current and next transitions with gsap

--- 02_canvas-magazine/scripts.js
+++ 02_canvas-magazine/scripts.js
@@ -33,21 +33,33 @@ barba.init({
         return new Promise(resolve => {
           const timeline = gsap.timeline({
             onComplete() {
+              current.container.remove()
               resolve()
             }
           })
-          timeline.to("header", { y: "-100%" }, 0)
+          timeline
+            .to("header", { y: '-100%' }, 0)
+            .to('footer', { y: '100%'}, 0)
+            .to(current.container, { opacity: 0})
         })
       },
       enter({ current, next, trigger }) {
         return new Promise(resolve => {
+
+          window.scrollTo({top:0, behavior:'smooth'})
+
           const timeline = gsap.timeline({
             onComplete() {
               runScripts() //corre despues del timeline
               resolve()
             }
           })
-          timeline.to("header", { y: "0" }, 0)
+
+          timeline
+            .set(next.container, { opacity: 0}) 
+            .to("header", { y: '0%' }, 0)
+            .to('footer', { y: '0%'}, 0)
+            .to(next.container, { opacity: 1})
         })
       }      
     }
